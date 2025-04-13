6fd221dbe872c8fe2e8574fe76514420798250ac
Charity <chaaritee@gmail.com>
2025-04-13 23:58:32 +0100

    Add basic request handler with route matching for GET /

--- server/src/main.rs
+++ server/src/main.rs
@@ -1,6 +1,29 @@
 use hyper::{Body, Request, Response, Server, Method, StatusCode};
 use hyper::service::{make_service_fn, service_fn};
+use std::convert::Infallible;
 use tokio::fs;
+use std::path::Path;
+
+async fn handle_request(req: Request<Body>) -> Result<Response<Body>, Infallible> {
+    match (req.method(), req.uri().path()) {
+        (&Method::GET, "/") => {
+            Ok(Response::new(Body::from("Welcome to the Secure Rust Server!")))
+        }
+        (&Method::GET, path) => {
+            let file_path = format!(".{}", path);
+            if Path::new(&file_path).exists() {
+                match fs::read_to_string(file_path).await {
+                    Ok(contents) => Ok(Response::new(Body::from(contents))),
+                    Err(_) => Ok(Response::builder()
+                        .status(StatusCode::INTERNAL_SERVER_ERROR)
+                        .body(Body::from("Error reading file"))
+                        .unwrap()),
+                }
+                .body(Body::from("405 - Method Not Allowed"))
+                .unwrap())
+        }
+    }
+}
 
 #[tokio::main]
 async fn main() {
