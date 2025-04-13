7d56b493b4662e4120d3f0cf6ff4f2fdc017aa50
Charity <chaaritee@gmail.com>
2025-04-13 23:55:39 +0100

    Initialize basic Hyper server with async main function

--- server/src/main.rs
+++ server/src/main.rs
@@ -1,3 +1,17 @@
-fn main() {
-    println!("Hello, world!");
+use hyper::{Body, Request, Response, Server, Method, StatusCode};
+use hyper::service::{make_service_fn, service_fn};
+use tokio::fs;
+
+#[tokio::main]
+async fn main() {
+    let addr = ([127, 0, 0, 1], 8080).into();
+    let service = make_service_fn(|_| async { Ok::<_, Infallible>(service_fn(handle_request)) });
+
+    let server = Server::bind(&addr).serve(service);
+
+    println!("Server running on http://{}", addr);
+    if let Err(e) = server.await {
+        eprintln!("Server error: {}", e);
+    }
 }
+
